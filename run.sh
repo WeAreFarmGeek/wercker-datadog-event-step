1ac4e9e4bff7124d7ea5f924cfe8876984bd2d2b
John Hamelink <hello@farmer.io>
2014-07-27 22:17:58 +0100

    Typo!

--- run.sh
+++ run.sh
@@ -26,9 +26,9 @@ fi
 
 curl  -X POST -H "Content-type: application/json" \
 -d '{
-      "title":       "$WERCKER_CAMPFIRE_NOTIFY_TITLE",
-      "text":        "$WERCKER_CAMPFIRE_NOTIFY_TEXT",
-      "priority":    "$WERCKER_CAMPFIRE_NOTIFY_PRIORITY",
-      "alert_type":  "$WERCKER_CAMPFIRE_NOTIFY_ALERT_TYPE"
+      "title":       "$WERCKER_DATADOG_EVENT_TITLE",
+      "text":        "$WERCKER_DATADOG_EVENT_TEXT",
+      "priority":    "$WERCKER_DATADOG_EVENT_PRIORITY",
+      "alert_type":  "$WERCKER_DATADOG_EVENT_ALERT_TYPE"
   }' \
-'https://app.datadoghq.com/api/v1/events?api_key=$WERCKER_CAMPFIRE_NOTIFY_TOKEN'
+'https://app.datadoghq.com/api/v1/events?api_key=$WERCKER_DATADOG_EVENT_TOKEN'
